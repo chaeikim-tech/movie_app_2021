e2340a36843a3be21ba64ebd2427c0b82eab9a0a
eunjukim-tech <76060912+eunjukim-tech@users.noreply.github.com>
2021-07-30 11:24:44 +0900

    Make Detail Page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -14,7 +14,7 @@ class Home extends React.Component {
         data: { movies }
       }
     } = await axios.get(
-      "https://yts-proxy.now.sh/list_movies.json"
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
     );
     this.setState({ movies, isLoading: false });
   };
